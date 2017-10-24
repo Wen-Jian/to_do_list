95dcba86adf342e5949a36b901d2092e3a5f559a
Wen <genning7@gmail.com>
2017-10-24 18:09:44 +0800

    CRUD完成，待完成時間過無法編輯與加註每個項目狀態

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,11 +1,16 @@
 class ListsController < ApplicationController
-
+	before_action :set_list, :only => [:edit, :update, :destroy]
 	def index
-
+		@time = Time.now
 		@lists = List.all
 		@select = Selectparam.find(1)
-		@list = List.find(@select[:params])
-		
+		if @select[:params] != 0 
+			if List.find(@select[:params])!= nil
+				@list = List.find(@select[:params])
+			else
+				@list = nil
+			end
+		end
 	end
 	def new
 
@@ -14,13 +19,21 @@ class ListsController < ApplicationController
 
 	end
 	def create
-
+		
+		e = params[:list]
+		e[:notes] = e[:notes].split(%r{\r\n*})
+		
 		@list = List.new(list_params)
+
+
+		
 		if @list.save 
 			redirect_to lists_path
 		else
 			render :action => :new
 		end
+
+
 	end
 	def show
 		@selectparams = Selectparam.first
@@ -29,14 +42,42 @@ class ListsController < ApplicationController
 		redirect_to lists_path
 		
 	end
-	
+	def edit
+		
+	end
+	def update
+		if @list.update_attributes(list_params)
+			redirect_to lists_path
+		else
+			render :action => :edit
+		end
+		
+	end
+
+	def destroy
+
+		@list.destroy
+		@select = Selectparam.first
+		@select[:params] = 0
+
+		redirect_to lists_path
+		
+	end
+
 	private
 
 	def list_params
 
-		params.require(:list).permit(:title, :due, :notes)
+		params.require(:list).permit(:title, :due, :notes => [])
+		
+
+	end
+	
+	def set_list
+		@list = List.find(params[:id])
 		
 	end
+
 
 end
 
