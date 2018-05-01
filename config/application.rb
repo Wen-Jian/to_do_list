5a9b7c725ecfefd1f7b13b58db3b369d74ee6dc2
Wen <genning7@gmail.com>
2018-05-01 18:42:23 +0800

    add cors allowence

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,17 @@ module ToDoList
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.middleware.insert_before 0, Rack::Cors do 
+    	allow do
+    		origins 'localhost:4200'
+    		resource '*',
+    			headers: :any,
+    			methods: %i(get post put patch delete options head)
+    		
+    	end
+    end
+
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
